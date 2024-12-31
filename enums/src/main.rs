466541781c08abe401ddd31f33060f9f67d85a66
Micah Shallom Bawa <micahshallom@gmail.com>
2024-12-31 14:53:30 +0100

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,75 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+let home = IpAddr {
+    kind: IpAddrKind::V4,
+    address: String::from("127.0.0.1"),
+}
+
+let loopback = IpAddr {
+    kind: IpAddrKind::V6,
+    address: String::from("::1"),
+}
+
+//we can hve the enum be defined in itself without needing a struct
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+let home = IpAddr::V4(String::from("127.0.0.1"));
+let loopback = IpAddr::V6(String::from("::1"));
+
+//enums can take in different types and amountsof associated data
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+let home = IpAddr::V4(127,0,0,1);
+let loopback = IpAddr::V6(String::from("::1"));
+
+// =========================================
+struct Ipv4Addr {
+}
+
+struct Ipv6Addr {
+}
+
+enum IpAddr {
+    V4(Ipv4Addr),
+    V6(Ipv6Addr),
+}
+// ===========================================
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32,i32,i32),
+}
+
+struct QuitMessage;
+struct MoveMessage{
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String);
+struct ChangeColorMessage(i32, i32, i32);
+
+enum Option<T> {
+    None, 
+    Some(T),
+}
+
+let some_number = Some(5);
+let some_char = Some('e);
+
+let absent_number: Option<i32> = None;
+
