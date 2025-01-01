fbad363127aa758370eec1b0d462e50259a971bb
Micah Shallom Bawa <micahshallom@gmail.com>
2025-01-01 18:54:39 +0100

    crates

---
+++ crates/target/debug/deps/crates-dd225cb212bc6147.d
@@ -0,0 +1,5 @@
+/home/mshallom/Documents/WorkSpaces/Rust-Programming-Learning/crates/target/debug/deps/libcrates-dd225cb212bc6147.rmeta: src/main.rs
+
+/home/mshallom/Documents/WorkSpaces/Rust-Programming-Learning/crates/target/debug/deps/crates-dd225cb212bc6147.d: src/main.rs
+
+src/main.rs:
---
+++ crates/target/debug/deps/crates-e6ade47ec980b79c.d
@@ -0,0 +1,5 @@
+/home/mshallom/Documents/WorkSpaces/Rust-Programming-Learning/crates/target/debug/deps/libcrates-e6ade47ec980b79c.rmeta: src/lib.rs
+
+/home/mshallom/Documents/WorkSpaces/Rust-Programming-Learning/crates/target/debug/deps/crates-e6ade47ec980b79c.d: src/lib.rs
+
+src/lib.rs:
